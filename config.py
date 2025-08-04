302d3c61c3bec9ef7d0df34e4df2f88dc0588b3b
Ravisindhav <sindhavravi11@gmail.com>
2025-08-04 21:12:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 api_id = int(os.environ.get("27660379"))
-api_hash = os.environ.get("api_hash")
-bot_token = os.environ.get("bot_token")
+api_hash = os.environ.get("19c71c27733f0954371085198855125a")
+bot_token = os.environ.get("8389662901:AAGN1Ujj_guB4xTRLXw9hAysT6ufSVJlt3U")
 
 auth_users = [5459854363]  # अपना Telegram user ID डालो
 sudo_users = [5459854363]
