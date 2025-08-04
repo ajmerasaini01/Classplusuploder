e9c229e11098d474a01bbb69c3a5078834f87dc3
Ravisindhav <sindhavravi11@gmail.com>
2025-08-04 21:19:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
 import os
 
-api_id = int(os.environ.get("27660379"))
-api_hash = os.environ.get("19c71c27733f0954371085198855125a")
-bot_token = os.environ.get("8389662901:AAGN1Ujj_guB4xTRLXw9hAysT6ufSVJlt3U")
-
-auth_users = [5459854363]  # अपना Telegram user ID डालो
-sudo_users = [5459854363]
+api_id = int(os.environ.get("api_id"))
+api_hash = os.environ.get("api_hash")
+bot_token = os.environ.get("bot_token")
+auth_users = set(map(int, os.environ.get("auth_users", "").split()))
+sudo_users = set(map(int, os.environ.get("sudo_users", "").split()))
