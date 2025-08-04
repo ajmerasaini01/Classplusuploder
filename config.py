4642570d244a314d43b2e5d469ba975daab7c839
Ravisindhav <sindhavravi11@gmail.com>
2025-08-04 19:04:38 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+api_id = int(os.environ.get("api_id"))
+api_hash = os.environ.get("api_hash")
+bot_token = os.environ.get("bot_token")
+
+auth_users = [5459854363]  # अपना Telegram user ID डालो
+sudo_users = [5459854363]
